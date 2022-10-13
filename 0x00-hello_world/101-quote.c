b0f47e58eadeadb0798301bb920ebd64707669a3
Virtue3110 <moshoodharbiolar31@gmail.com>
2022-10-13 06:51:45 -0700

    task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,13 @@
+#include <stdin.h>
 #include <unistd.h>
 
 /**
- *
  * main - Print entry
  *
  * Return: Always 1 (Success)
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-	return (0);
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
